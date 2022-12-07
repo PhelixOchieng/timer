8798a028193165c880e53f2c9843130760bf04f0
Phelix <dev.phelixochieng@gmail.com>
2022-12-07 14:52:10 +0300

    Add time selections

--- lib/app/features/home/widgets/dial.dart
+++ lib/app/features/home/widgets/dial.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:color_fns/color_fns.dart';
 import 'package:flutter/material.dart';
@@ -29,6 +30,7 @@ class Dial extends HookWidget {
     final isSelecting =
         isSelectingSeconds || isSelectingMinutes || isSelectingHours;
 
+    final dialRotationAngle = useState(0.0);
     final timer = useRef<Timer?>(null);
     final isTimerRunning = timer.value != null;
     void stopTimer() {
@@ -38,6 +40,7 @@ class Dial extends HookWidget {
 
       timeSelection.value = TimeSelection.seconds;
       countdownSeconds.value = 0;
+      dialRotationAngle.value = 0;
       selectedSeconds.value = selectedMinutes.value = selectedHours.value = 0;
     }
 
@@ -48,7 +51,6 @@ class Dial extends HookWidget {
           (selectedMinutes.value * 60) +
           selectedSeconds.value;
       timer.value = Timer.periodic(const Duration(seconds: 1), (_) {
-        debugPrint('Count: ${countdownSeconds.value}');
         countdownSeconds.value -= 1;
         final remainingSeconds = countdownSeconds.value;
 
@@ -61,12 +63,34 @@ class Dial extends HookWidget {
       });
     }
 
+    useEffect(() {
+      switch (timeSelection.value) {
+        case TimeSelection.seconds:
+          dialRotationAngle.value = (selectedSeconds.value / 60) * (2 * pi);
+          break;
+        case TimeSelection.minutes:
+          dialRotationAngle.value = (selectedMinutes.value / 60) * (2 * pi);
+          break;
+        case TimeSelection.hours:
+          dialRotationAngle.value = (selectedHours.value / 24) * (2 * pi);
+          break;
+        case null:
+      }
+
+      debugPrint('${dialRotationAngle.value} - ${timeSelection.value}');
+
+      return;
+    }, [timeSelection.value]);
+
     final theme = Theme.of(context);
     final textTheme = theme.textTheme;
 
     final availableWidth =
         MediaQuery.of(context).size.width - kAppPadding.horizontal;
     final dialSize = availableWidth * 0.75;
+    const detentMargin = 10.0;
+    final paddedDialSize = dialSize - (detentMargin * 2);
+    const detentSize = 24.0;
 
     return GestureDetector(
       onTapDown: (_) => pressedAnimationController.reverse(),
@@ -83,68 +107,153 @@ class Dial extends HookWidget {
           startCountdown();
         }
       },
+      onPanUpdate: (details) {
+        final pos = details.localPosition;
+
+        final zeroX = dialSize / 2;
+        final zeroY = dialSize / 2;
+        final resolvedX = pos.dx - zeroX;
+        final resolvedY = -(pos.dy - zeroY);
+
+        double angle = atan(resolvedX / resolvedY);
+
+        if (resolvedX >= 0 && resolvedY < 0) {
+          angle = (pi / 2) + ((pi / 2) - angle.abs());
+        } else if (resolvedX < 0 && resolvedY < 0) {
+          angle += pi;
+        } else if (resolvedX <= 0 && resolvedY > 0) {
+          angle = pi + ((pi / 2) + ((pi / 2) - angle.abs()));
+        }
+
+        if (angle == 2 * pi) angle = 0;
+
+        // debugPrint('$angle ${angle * (180 / pi)} ${angleDeg % 6}');
+        dialRotationAngle.value = angle;
+
+        final angleDeg = (angle * (180 / pi)).floor();
+        if (isSelectingSeconds) {
+          selectedSeconds.value = (angleDeg * (60 / 360)).floor();
+        } else if (isSelectingMinutes) {
+          selectedMinutes.value = (angleDeg * (60 / 360)).floor();
+        } else if (isSelectingHours) {
+          selectedHours.value = (angleDeg * (24 / 360)).floor();
+        }
+      },
+      onPanEnd: (_) {
+        double angle = dialRotationAngle.value;
+        final angleDeg = (angle * (180 / pi)).floor();
+
+        double angleChange = 0;
+        if (isSelectingSeconds || isSelectingMinutes) {
+          angleChange = 360 / 60;
+        } else if (isSelectingHours) {
+          angleChange = 360 / 24;
+        }
+
+        if (angleDeg % angleChange == 0) return;
+
+        angle -= (angleDeg % angleChange) * ((2 * pi) / 360);
+        dialRotationAngle.value = angle;
+      },
       child: Transform.scale(
         scale: pressedAnimation,
-        child: Container(
-          width: dialSize,
-          height: dialSize,
-          decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: theme.scaffoldBackgroundColor,
-              boxShadow: [
-                ..._topLeftShadows(theme),
-                ..._bottomRightShadows(theme),
-              ]),
-          child: Center(
-            child: Padding(
-                padding: const EdgeInsets.all(32),
-                child: isSelecting
-                    ? Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          DefaultTextStyle(
-                              style: textTheme.headline1!.copyWith(),
-                              child: isSelectingSeconds
-                                  ? Text('${selectedSeconds.value}')
-                                  : isSelectingMinutes
-                                      ? Text('${selectedMinutes.value}')
-                                      : Text('${selectedHours.value}')),
-                          const SizedBox(height: 12),
-                          Text(
-                              'Select ${isSelectingSeconds ? 'Seconds' : isSelectingMinutes ? 'Minutes' : 'Hours'}'),
-                        ],
-                      )
-                    : Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const SizedBox(height: 12),
-                          FittedBox(
-                            child: DefaultTextStyle(
-                              style: textTheme.headline1!,
-                              child: Row(
-                                children: [
-                                  Text(_parseTime(selectedHours.value)),
-                                  const Text(':'),
-                                  Text(_parseTime(selectedMinutes.value)),
-                                  const Text(':'),
-                                  Text(_parseTime(selectedSeconds.value)),
-                                ],
+        child: Stack(
+          children: [
+            Container(
+              width: dialSize,
+              height: dialSize,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: theme.scaffoldBackgroundColor,
+                  boxShadow: [
+                    ..._topLeftShadows(theme),
+                    ..._bottomRightShadows(theme),
+                  ]),
+              child: Center(
+                child: Padding(
+                    padding: const EdgeInsets.all(32),
+                    child: isSelecting
+                        ? Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              DefaultTextStyle(
+                                  style: textTheme.headline1!.copyWith(),
+                                  child: isSelectingSeconds
+                                      ? Text('${selectedSeconds.value}')
+                                      : isSelectingMinutes
+                                          ? Text('${selectedMinutes.value}')
+                                          : Text('${selectedHours.value}')),
+                              const SizedBox(height: 12),
+                              Text(
+                                  'Select ${isSelectingSeconds ? 'Seconds' : isSelectingMinutes ? 'Minutes' : 'Hours'}'),
+                            ],
+                          )
+                        : Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const SizedBox(height: 32),
+                              if (!isTimerRunning) const SizedBox(height: 32),
+                              FittedBox(
+                                child: DefaultTextStyle(
+                                  style: textTheme.headline1!,
+                                  child: Row(
+                                    children: [
+                                      Text(_parseTime(selectedHours.value)),
+                                      const Text(':'),
+                                      Text(_parseTime(selectedMinutes.value)),
+                                      const Text(':'),
+                                      Text(_parseTime(selectedSeconds.value)),
+                                    ],
+                                  ),
+                                ),
                               ),
-                            ),
-                          ),
-                          if (!isTimerRunning) const SizedBox(height: 12),
-                          !isTimerRunning
-                              ? const Text(
-                                  'Tap To Start',
-                                  textAlign: TextAlign.center,
+                              if (!isTimerRunning) ...[
+                                const SizedBox(height: 12),
+                                const Text('Tap To Start',
+                                    textAlign: TextAlign.center),
+                                const SizedBox(height: 4),
+                                InkWell(
+                                  onTap: () {},
+                                  child: TextButton.icon(
+                                    onPressed: stopTimer,
+                                    icon: const Icon(Icons.restart_alt),
+                                    label: Text('Reset',
+                                        textAlign: TextAlign.center),
+                                  ),
                                 )
-                              : TextButton.icon(
-                                  onPressed: stopTimer,
-                                  icon: const Icon(Icons.stop_rounded),
-                                  label: Text('Stop Timer'))
-                        ],
-                      )),
-          ),
+                              ],
+                              if (isTimerRunning)
+                                TextButton.icon(
+                                    onPressed: stopTimer,
+                                    icon: const Icon(Icons.stop_rounded),
+                                    label: Text('Stop Timer'))
+                            ],
+                          )),
+              ),
+            ),
+            if (isSelecting)
+              Positioned(
+                left: (dialSize / 2) - (detentSize / 2),
+                top: detentMargin,
+                height: paddedDialSize,
+                // width: detentSize,
+                child: Transform.rotate(
+                  angle: dialRotationAngle.value,
+                  child: Column(
+                    children: [
+                      Container(
+                        width: detentSize,
+                        height: detentSize,
+                        decoration: BoxDecoration(
+                          color: theme.primaryColor,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+          ],
         ),
       ),
     );
